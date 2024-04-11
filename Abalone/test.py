7206071d670485e3a9b57fcce174a1b40a432f8d
Victor Blanquez <victor.blanquez-yeste@polymtl.ca>
2024-04-11 17:02:24 -0400

    Better "test.py" + new "analyse.py"
    test.py : calculates the closest to the center to determine equalities.
    
    analse.py : donne aussi les stats pour des games json storées dans un folder

--- Abalone/test.py
+++ Abalone/test.py
@@ -2,6 +2,11 @@ import subprocess
 import os
 import json
 import shutil
+import pickle
+
+# Chargement du dictionnaire s'il n'a pas encore été chargé.
+with open('src_2226611_2225992/abalone_distances.pkl', 'rb') as f:
+    all_distances = pickle.load(f)
 
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -30,15 +35,42 @@ def open_json_files_in_folder():
         shutil.move(json_file, destination_path)
     return all_files
 
+def distance_to_center(player_pieces):
+    """
+    Calcule la distance totale des pièces du joueur par rapport au centre.
+
+    Args:
+        player_pieces (list): Liste des coordonnées des pièces du joueur.
+
+    Returns:
+        float: Distance normalisée par rapport au centre.
+    """
+    central_position = (8, 4)
+    length_to_center = sum(all_distances[(coord, central_position)] if coord != central_position else 0 for coord in player_pieces)
+    max_distance = 4 * len(player_pieces)
+    return length_to_center
+
+def get_players_pieces(last_turn):
+    player_1_pieces = []
+    player_2_pieces = []
+    board = last_turn["rep"]["env"]
+    for coord in board:
+        if board[coord]["piece_type"] == 'W':
+            player_1_pieces.append(eval(coord))
+        elif board[coord]["piece_type"] == 'B':
+            player_2_pieces.append(eval(coord))
+    return player_1_pieces, player_2_pieces
+
 
 # play with this
-nbr_of_games = 100
-turn = 0 # 0 if first (player1), 1 if second (player 2)
-command = "python main_abalone.py -t local my_player.py greedy_player_abalone.py -c alien -r -g"
+nbr_of_games = 25
+command = "python main_abalone.py -t local random_player_abalone.py my_player.py -c alien -r -g"
+turn = 1 # 0 if first (player1), 1 if second (player 2)
 
 # simulate n games with command line
 for i in range(nbr_of_games):
     output, error = run_command(command)
+    print(i)
     #if error:
         #print("Error:", error)
 
@@ -46,7 +78,8 @@ for i in range(nbr_of_games):
 games = open_json_files_in_folder()
 won = 0
 lost = 0
-none = 0
+score_equal = 0
+closer = 0
 for game in games:
     last_turn = game[-1]
     #get player id
@@ -56,21 +89,38 @@ for game in games:
     score_1 = last_turn["scores"][str(player_1_id)]
     score_2 = last_turn["scores"][str(player_2_id)]
     if score_1>score_2:
-        won +=1
+        if turn==0:
+            won +=1
+        elif turn==1:
+            lost +=1
     elif score_1<score_2:
-        lost +=1
+        if turn==0:
+            lost +=1
+        elif turn==1:
+            won +=1
     else:
-        none +=1
+        score_equal +=1
+        pieces1, pieces2 = get_players_pieces(last_turn)
+        if distance_to_center(pieces1)<distance_to_center(pieces2):
+            if turn==0:
+                closer +=1 
+        else:
+            if turn==1:
+                closer +=1
         
 #print stats
 pure_win_per = won/len(games)
+win_per = (won+closer)/len(games)
 pure_loss_per = lost/len(games)
-none_perc = none/len(games)
+loss_per = (lost+(score_equal-closer))
+if score_equal != 0:
+    closer_perc = closer/score_equal
+
 
 print("STATS :")
 print("Pure win percerntage : "+ str(pure_win_per), " Games won : " + str(won)+"/"+str(len(games)))
 print("Pure loss percerntage : "+ str(pure_loss_per), " Games lost : " + str(lost)+"/"+str(len(games)))
-print("Equalities: "+ str(none) +" (real winner is the one closer to the center)")
+print("Equalities: "+ str(score_equal) + ", " + str(closer)+"/" +str(score_equal)+ " won by being closer")
 
 
 
